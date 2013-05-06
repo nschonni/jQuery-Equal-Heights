fdc5bcff14fdf9db229deded7de7f262984a0c16
Nick Schonning <nschonni@gmail.com>
2013-05-06 15:23:07 -0400

    Fix indenting and IE6 conditional path

--- jQuery.equalHeights.js
+++ jQuery.equalHeights.js
@@ -18,30 +18,49 @@
 --------------------------------------------------------------------*/
 
 $.fn.equalHeights = function(px) {
-	$(this).each(function(){
+	$(this).each(function() {
 		var currentTallest = 0;
-		$(this).children().each(function(i){
-			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+		$(this).children().each(function(i) {
+			if ($(this).height() > currentTallest) {
+				currentTallest = $(this).height();
+			}
 		});
-    if (!px && Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
+		
+		//use ems unless px is specified
+		if (!px && Number.prototype.pxToEm) {
+			currentTallest = currentTallest.pxToEm();
+		}
+		
 		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'height': currentTallest}); }
-		$(this).children().css({'min-height': currentTallest}); 
+		if ($.browser.msie && $.browser.version == 6.0) {
+			$(this).children().css({'height': currentTallest});
+		} else {
+			$(this).children().css({'min-height': currentTallest});
+		}
 	});
 	return this;
 };
 
 // just in case you need it...
 $.fn.equalWidths = function(px) {
-	$(this).each(function(){
+	$(this).each(function() {
 		var currentWidest = 0;
-		$(this).children().each(function(i){
-				if($(this).width() > currentWidest) { currentWidest = $(this).width(); }
+		$(this).children().each(function(i) {
+				if($(this).width() > currentWidest) {
+					currentWidest = $(this).width();
+				}
 		});
-		if(!px && Number.prototype.pxToEm) currentWidest = currentWidest.pxToEm(); //use ems unless px is specified
+		
+		//use ems unless px is specified
+		if(!px && Number.prototype.pxToEm) {
+			currentWidest = currentWidest.pxToEm();
+		}
 		// for ie6, set width since min-width isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'width': currentWidest}); }
-		$(this).children().css({'min-width': currentWidest}); 
+		if ($.browser.msie && $.browser.version == 6.0) {
+			$(this).children().css({'width': currentWidest});
+		} else {
+			$(this).children().css({'min-width': currentWidest});
+		}
 	});
 	return this;
 };
